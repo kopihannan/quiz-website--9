f6f22a9dc563c04ce377b4d10dfb0673eee1a05e
kopihannan <kopihannan3@gmail.com>
2022-10-12 21:53:01 +0600

    added text topic section and done work

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -3,15 +3,20 @@ import { useLoaderData } from 'react-router-dom';
 import QuizContainer from './QuizContainer';
 import './Quiz.css'
 const Quiz = () => {
-    const quizs = useLoaderData();
-    const allQuiz = quizs.data;
-    return (
-        <div className='quiz-container my-14'>
-          {
-            allQuiz.map(quiz=> <QuizContainer key={quiz.id} quiz={quiz}></QuizContainer>)
-          }
-        </div>
-    );
+  const quizs = useLoaderData();
+  const allQuiz = quizs.data;
+  return (
+    <div>
+      <div className='flex justify-center mt-3'>
+        <p className='w-3/5 text-justify font-medium'>“Don’t ever let someone tell you, you can’t do something. Not even me. You got a dream, you got to protect it. People can’t do something themselves, they want to tell you you can’t do it. You want something, go get it. Period.”</p>
+      </div>
+      <div className='quiz-container my-14'>
+        {
+          allQuiz.map(quiz => <QuizContainer key={quiz.id} quiz={quiz}></QuizContainer>)
+        }
+      </div>
+    </div>
+  );
 };
 
 export default Quiz;
